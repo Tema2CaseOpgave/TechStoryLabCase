7ea9bf1e5846dd8f87dc4c184b11598af6bf2f22
ElMaez <mai_ez@outlook.dk>
2024-09-16 20:28:28 +0200

    gået igang med js, mangler at finde ud af hvordan jeg tilføjer id til fetch kriteriet ligesom på forrige opgave.

---
+++ js/singleview.js
@@ -0,0 +1,25 @@
+window.addEventListener("DOMContentLoaded", init);
+
+const urlParams = new URLSearchParams(window.location.search);
+// const id = urlParams.get("id");
+
+const id = "C00332";
+
+function init() {
+  fetch("https://inrchbadbkirfzkcebcg.supabase.co/rest/v1/TSLNEW", {
+    method: "GET",
+    headers: {
+      apikey:
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlucmNoYmFkYmtpcmZ6a2NlYmNnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU5NzA4MjYsImV4cCI6MjA0MTU0NjgyNn0.mpSt_VfMJynts4yNhF_lHvJafqeHolxXL7wYKBacfjA",
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => showProduct(data));
+}
+
+function showProduct(items) {
+  console.log(items);
+  // items.forEach(items);
+
+  document.querySelector("h2").textContent = items.ProductName;
+}
