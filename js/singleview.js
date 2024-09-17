dbc5c26e5e09ad65d2745d6d68d79d6f514873d3
ElMaez <mai_ez@outlook.dk>
2024-09-17 09:49:10 +0200

    mangler hjælp til skaffelse af dynamisk id

--- js/singleview.js
+++ js/singleview.js
@@ -1,18 +1,22 @@
 window.addEventListener("DOMContentLoaded", init);
 
-const urlParams = new URLSearchParams(window.location.search);
+// const urlParams = new URLSearchParams(window.location.search);
 // const id = urlParams.get("id");
 
 const id = "C00332";
+console.log(id);
 
 function init() {
-  fetch("https://inrchbadbkirfzkcebcg.supabase.co/rest/v1/TSLNEW", {
-    method: "GET",
-    headers: {
-      apikey:
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlucmNoYmFkYmtpcmZ6a2NlYmNnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU5NzA4MjYsImV4cCI6MjA0MTU0NjgyNn0.mpSt_VfMJynts4yNhF_lHvJafqeHolxXL7wYKBacfjA",
-    },
-  })
+  fetch(
+    "https://inrchbadbkirfzkcebcg.supabase.co/rest/v1/TSLNEW?ID=cs.%5B%22id%22%5D",
+    {
+      method: "GET",
+      headers: {
+        apikey:
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlucmNoYmFkYmtpcmZ6a2NlYmNnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU5NzA4MjYsImV4cCI6MjA0MTU0NjgyNn0.mpSt_VfMJynts4yNhF_lHvJafqeHolxXL7wYKBacfjA",
+      },
+    }
+  )
     .then((res) => res.json())
     .then((data) => showProduct(data));
 }
