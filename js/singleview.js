fb67c8dfb02d5ab1ebb9187bc4abe779000eb6f2
ElMaez <mai_ez@outlook.dk>
2024-09-18 17:59:10 +0200

    our singleview is fucking done. still no css ofc

--- js/singleview.js
+++ js/singleview.js
@@ -7,26 +7,25 @@ const id = urlParams.get("number");
 console.log(id);
 
 function init() {
-  fetch(
-    `https://inrchbadbkirfzkcebcg.supabase.co/rest/v1/TSLNEW?number=eq.${id}`,
-    {
-      method: "GET",
-      headers: {
-        apikey:
-          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlucmNoYmFkYmtpcmZ6a2NlYmNnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU5NzA4MjYsImV4cCI6MjA0MTU0NjgyNn0.mpSt_VfMJynts4yNhF_lHvJafqeHolxXL7wYKBacfjA",
-      },
-    }
-  )
-    .then((res) => res.json())
-    .then((data) => showProduct(data));
-
-  fetch(`https://inrchbadbkirfzkcebcg.supabase.co/rest/v1/TSLNEW?limit=3`, {
+  const option = {
     method: "GET",
     headers: {
       apikey:
         "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlucmNoYmFkYmtpcmZ6a2NlYmNnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU5NzA4MjYsImV4cCI6MjA0MTU0NjgyNn0.mpSt_VfMJynts4yNhF_lHvJafqeHolxXL7wYKBacfjA",
     },
-  })
+  };
+
+  fetch(
+    `https://inrchbadbkirfzkcebcg.supabase.co/rest/v1/TSLNEW?number=eq.${id}`,
+    option
+  )
+    .then((res) => res.json())
+    .then((data) => showProduct(data));
+
+  fetch(
+    `https://inrchbadbkirfzkcebcg.supabase.co/rest/v1/TSLNEW?limit=3`,
+    option
+  )
     .then((resa) => resa.json())
     .then((dataa) => handleRec(dataa));
 }
